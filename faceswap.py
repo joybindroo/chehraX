64353e563e4f75e255e0f3ed82b58d2ee4ed476d
Nicolas Metallo <nicolas.metallo@nyu.edu>
2018-09-08 18:12:34 -0300

    update

--- faceswap.py
+++ faceswap.py
@@ -25,10 +25,10 @@ def url_to_image(url):
     # download the image, convert it to a NumPy array, and then read
 	# it into OpenCV format
     resp = urllib.urlopen(url)
-	image = np.asarray(bytearray(resp.read()), dtype="uint8")
-	image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 	# return the image
-	return image
+    return image
 
 def run_face_swap(from_image_path, to_image_path, output_path):
     """Switch faces between two input images using dlib and OpenCV."""
